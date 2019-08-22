5804122194797c27b6bd6b20fd16b899dcc6e822
Nate Tan <nktan93@gmail.com>
2019-08-22 17:46:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ function createFareMultiplier(multiplier) {
 const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, cb) {
+  return cb(drivers);
+}
