860f1239ece3700077dad4f1e5651a1df28d734a
tan-nate <nktan93@gmail.com>
2019-10-16 11:00:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
+import { create } from "domain";
+
 // Code your solution in this file!
-function returnFirstTwoDrivers(drivers) {
-  return drivers.slice(0, 2);
-}
 
-function returnLastTwoDrivers(drivers) {
-  return drivers.slice(-2);
-}
-
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-
-function createFareMultiplier(multiplier) {
-  return function (num) {
-    return num * multiplier;
-  }
-}
-
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+};
+  
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+};
+  
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+  
+const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+      return multiplyValue * value;
+    };
+};
+  
 const fareDoubler = createFareMultiplier(2);
-
+  
 const fareTripler = createFareMultiplier(3);
-
-function selectDifferentDrivers(drivers, cb) {
-  return cb(drivers);
-}
+  
+const selectDifferentDrivers = function (drivers, whichDrivers) {
+    return whichDrivers(drivers);
+};
